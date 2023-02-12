43b4c19ca73d4e2b3ec40066554292f93ce4e4d8
Hasan Huda <hasanhuda037@gmail.com>
2023-02-11 19:43:59 -0500

    first commit

---
+++ src/test/java/com/hasan/dojo/DojosNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasan.dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojosNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
